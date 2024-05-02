c032a3be56c15f4a402ace2b1576d801c9bd5813
henrynester@gmail.com <henrynester@gmail.com>
2024-05-02 17:01:38 -0400

    support json config

--- bender.py
+++ bender.py
@@ -4,11 +4,13 @@ import ezdxf
 from ezdxf import units
 import ezdxf.path
 import progress.bar
-
+import json, sys
 
 class Bender:  # uh oh
-    def __init__(self, track_sequence, floquet_unit_cell):
+    def __init__(self, track_sequence, floquet_unit_cell, fillet_radius=None):
         self.track_sequence, self.floquet_unit_cell = track_sequence, floquet_unit_cell
+        self.fillet_radius = fillet_radius
+
         self.create_dxf()
         
     def construct_tline(self):
@@ -50,20 +52,23 @@ class Bender:  # uh oh
         self.msp = self.doc.modelspace()
         self.doc.units = units.M
 
-    def write_bent_tline(self, fillet=False):
+    def write_bent_tline(self):
         bar = progress.bar.Bar('', max=self.n_floquet_cells)
         for n in range(self.n_floquet_cells):
             first=self.unit_cell_vertices_count*n
             last=first+self.unit_cell_vertices_count
             polygon = [(self.bent_xs[i], self.bent_ys[i]) for i in range(first,last)]
             
-            if fillet:
+            if self.fillet_radius is not None:
+                # create path object, fillet path at control vertices (noncollinear points)
+                # then convert to an lwpolyline dxf file entry and add to the file
                 p = ezdxf.path.from_vertices(polygon)
                 p.close()
-                pp = ezdxf.path.fillet(p.control_vertices(), radius=0.75e-6)
+                pp = ezdxf.path.fillet(p.control_vertices(), radius=self.fillet_radius)
                 out = ezdxf.path.to_lwpolylines((pp,))
                 self.msp.add_lwpolyline(next(out), close=True)
             else:
+                # no fillets: polygon point list goes straight into the dxf file
                 self.msp.add_lwpolyline(polygon, close=True)
             
             bar.next()
@@ -74,26 +79,23 @@ class Bender:  # uh oh
         self.doc.saveas(filename)
 
 if __name__ == '__main__':
-    filename = 'straight_gnd_10GHz.dxf'
-    wcenter = 8e-6
-    wline = 40e-6 - wcenter
-    wload = 80e-6 - wcenter
-    fishboneA, fishboneB = tline.FishboneUnitCell(4e-6, 2e-6, wline/2, wcenter, gnd_spacing=2e-6, interdigitate=False), \
-                            tline.FishboneUnitCell(4e-6, 2e-6, wload/2, wcenter, gnd_spacing=2e-6, interdigitate=False)
-    # wcenter = 8e-6
-    # wline = 40e-6 - wcenter
-    # fishboneA = tline.FishboneUnitCell(4e-6, 2e-6, wline/2, wcenter, gnd_spacing=2e-6, interdigitate=False)
+    if len(sys.argv) <= 1:
+        print('usage: python bender.py /path/to/config.json')
+        raise ValueError()
+
+    cfgdict=None
+    dxf_filename=None
+    with open(sys.argv[1]) as cfgfile:
+        cfgdict = json.load(cfgfile)
+        dxf_filename = cfgfile.name.split('.json')[0] + '.dxf'
+        print(cfgfile.name, cfgdict)
+
+    cfg_tline, cfg_track = cfgdict['tline'], cfgdict['track']
+
     floquet = tline.FloquetUnitCell()
-    floquet.append_fishbones(fishboneA, 71)
-    floquet.append_fishbones(fishboneB, 25)
-    floquet.append_fishbones(fishboneA, 142)
-    floquet.append_fishbones(fishboneB, 25)
-    floquet.append_fishbones(fishboneA, 139)
-    floquet.append_fishbones(fishboneB, 30)
-    floquet.append_fishbones(fishboneA, 68)
-
-
-
+    for w_loaded_line, n_fishbone_cell in zip(cfg_tline['w_loaded_lines'], cfg_tline['n_fishbone_cells']):
+        fishbone = tline.FishboneUnitCell(cfg_tline['fishbone_cell_length'], cfg_tline['w_center_line'], cfg_tline['w_fishbone_line'], w_loaded_line, cfg_tline['gnd_spacing'], cfg_tline['gnd_interdigitate'])
+        floquet.append_fishbones(fishbone, n_fishbone_cell)
 
     #Alec 10GHz
     # floquet.append_fishbones(fishboneA, 19)
@@ -114,28 +116,32 @@ if __name__ == '__main__':
     # floquet.append_fishbones(fishboneA, 39)
 
 
-    min_spacing = 5.5*floquet.cell_min_spacing()
-    turns = 5.94
-    final_angle = turns * 2 * np.pi
-
-    tline_compact_length = 25e-3
-
-    fermat1 = track.FermatSpiralTrack(turns, min_spacing, True)
-    fermat2 = track.FermatSpiralTrack(turns, min_spacing, False)
-    arc2 = fermat2.construct_suitable_ArcTrack()
-    arc1 = fermat1.construct_suitable_ArcTrack()
-    straight2 = arc2.construct_suitable_StraightTrack(tline_compact_length)
-    straight1 = arc1.construct_suitable_StraightTrack(tline_compact_length)
-
     trackseq = track.TrackSequence()
-    trackseq.append_track(straight2)
-    trackseq.append_track(arc2)
-    trackseq.append_track(fermat2)
-    trackseq.append_track(fermat1)
-    trackseq.append_track(arc1)
-    trackseq.append_track(straight1)
+    if cfg_track['style'] == 'fermat':
+        min_spacing = cfg_track['line_spacing_factor']*floquet.cell_min_spacing()
+        turns = cfg_track['n_turns']
+        final_angle = turns * 2 * np.pi
 
-    bender = Bender(trackseq, floquet)
+        tline_compact_length = cfg_track['compact_length']
+
+        fermat1 = track.FermatSpiralTrack(turns, min_spacing, True)
+        fermat2 = track.FermatSpiralTrack(turns, min_spacing, False)
+        arc2 = fermat2.construct_suitable_ArcTrack()
+        arc1 = fermat1.construct_suitable_ArcTrack()
+        straight2 = arc2.construct_suitable_StraightTrack(tline_compact_length)
+        straight1 = arc1.construct_suitable_StraightTrack(tline_compact_length)
+
+        trackseq.append_track(straight2)
+        trackseq.append_track(arc2)
+        trackseq.append_track(fermat2)
+        trackseq.append_track(fermat1)
+        trackseq.append_track(arc1)
+        trackseq.append_track(straight1)
+    elif cfg_track['style'] == 'singletary':
+        # square wave shape
+        pass
+
+    bender = Bender(trackseq, floquet, cfg_tline['fillet_radius'])
     print('construct_tline')
     bender.construct_tline()
     print('bend_tline')
@@ -153,5 +159,5 @@ if __name__ == '__main__':
     print(f'{floquet.cell_length()*1e6:.1f}um unit cell * {bender.n_floquet_cells:d} unit cells should = {trackseq.total_arclength()*1e6:.0f}um track length')
 
     print('write_dxf')
-    bender.write_dxf(filename)
+    bender.write_dxf(dxf_filename)
 
